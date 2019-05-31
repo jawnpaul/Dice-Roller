d57618551a883bb055380d91fba133e76988c32d
John Paul <jpaul5246@gmail.com>
2019-05-31 16:58:44 +0100

    Made the diceIMageView a lateinit var for optimization purposes

--- app/src/main/java/ng/org/knowit/diceroller/MainActivity.kt
+++ app/src/main/java/ng/org/knowit/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package ng.org.knowit.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -10,19 +11,34 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
 
-        rollDice()
+        diceImage = findViewById(R.id.diceImageView)
+
     }
 
     private fun rollDice() {
-        TODO("not implemented")
-        //To change body of created functions use File | Settings | File Templates.
-        val randomInt = Random(6)
+
+        val rand = java.util.Random().nextInt(6) + 1
+
+        val diceImageResource = when(rand){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(diceImageResource)
 
 
     }
