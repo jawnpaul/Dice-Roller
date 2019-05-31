ef726f46c8fe905e5fd94335a171f5e81368249a
John Paul <jpaul5246@gmail.com>
2019-05-31 16:20:56 +0100

    Added the dice vector drawables

--- app/src/main/java/ng/org/knowit/diceroller/MainActivity.kt
+++ app/src/main/java/ng/org/knowit/diceroller/MainActivity.kt
@@ -2,11 +2,28 @@ package ng.org.knowit.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll"
+
+        rollDice()
+    }
+
+    private fun rollDice() {
+        TODO("not implemented")
+        //To change body of created functions use File | Settings | File Templates.
+        val randomInt = Random(6)
+
+
     }
 }
